5a2063f09a9f48411d5e7ad89570b0d8c8d19312
ConorCrutchley <conorc1993@hotmail.co.uk>
2025-07-31 07:22:41 +0100

    feat: Added card to transactions page

--- src/routes/_authed/dashboard/transactions/new/_layout.index.tsx
+++ src/routes/_authed/dashboard/transactions/new/_layout.index.tsx
@@ -1,9 +1,23 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 
-export const Route = createFileRoute('/_authed/dashboard/transactions/new/_layout/')({
+import TransactionForm from "@/components/TransactionForm";
+import { createFileRoute } from "@tanstack/react-router";
+
+export const Route = createFileRoute(
+  "/_authed/dashboard/transactions/new/_layout/"
+)({
   component: RouteComponent,
-})
+});
 
 function RouteComponent() {
-  return <div>Hello "/_authed/dashboard/transactions/new/"!</div>
+  return (
+    <Card className="max-w-screen-md mt-4">
+      <CardHeader>
+        <CardTitle>New Transaction</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <TransactionForm />
+      </CardContent>
+    </Card>
+  );
 }
